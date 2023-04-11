b9cc1b7d6a93e57d3957c1303b317376785afdcc
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-04-11 13:31:21 +0300

    Trimming Succesful, wip.

--- src/validation/msgGenerationUtil.js
+++ src/validation/msgGenerationUtil.js
@@ -8,33 +8,31 @@ const getKeyByValue = (object, value) => {
 //msgIncludeIndicatorArr = [0,0,0,1] => only string.pattern.base etc
 const generateMessages = ( msgSubject, msgLimitVarArr, msgType, msgIncludeIndicatorArr) => {
     const stringMsgTypeArr = {"string.min": 0, "string.max": 1, "string.empty": 2, "string.pattern.base": 3};
-    const stringMsgs = ["X should be at least Y Z long", "X should be at most Y Z long", "X cannot be empty", "X should meet rules"]
-    const numberMsgTypeArr = ["number.min", "number.max"];
+    const stringMsgs = ["XX should be at least YY ZZ long", "XX should be at most YY ZZ long", "XX cannot be empty", "XX should meet rules"]
+    const numberMsgTypeArr = {"number.min": 0, "number.max": 1};
     const typeUnits = ["characters", "digits"];
     const msgTypes = {"string" : 0, "number": 1};
     let msgObj = {};
     
     for(let msgIncludeIdx = 0; msgIncludeIdx < msgIncludeIndicatorArr.length; msgIncludeIdx++){
         if(msgType === msgTypes.string && msgIncludeIndicatorArr[msgIncludeIdx] && (msgIncludeIdx === stringMsgTypeArr["string.min"] || msgIncludeIdx === stringMsgTypeArr["string.max"])){
-        let validationMsg = stringMsgs[msgIncludeIdx];
-
-        validationMsg = validationMsg.replace("X", msgSubject);
-        validationMsg = validationMsg.replace("Y", msgLimitVarArr[msgIncludeIdx]);
-        validationMsg = validationMsg.replace("Z", typeUnits[msgTypes.string]);
-                console.log(validationMsg);
-        msgObj[getKeyByValue(stringMsgTypeArr, msgIncludeIdx)] = validationMsg;
+            let validationMsg = stringMsgs[msgIncludeIdx];
+            validationMsg = validationMsg.replace("XX", msgSubject);
+            validationMsg = validationMsg.replace("YY", msgLimitVarArr[msgIncludeIdx]);
+            validationMsg = validationMsg.replace("ZZ", typeUnits[msgTypes.string]);
+            msgObj[getKeyByValue(stringMsgTypeArr, msgIncludeIdx)] = validationMsg;
         }
         if(msgType === msgTypes.string && msgIncludeIndicatorArr[msgIncludeIdx] && !(msgIncludeIdx === stringMsgTypeArr["string.min"] || msgIncludeIdx === stringMsgTypeArr["string.max"])){
-        let validationMsg = stringMsgs[msgIncludeIdx];
-        validationMsg = validationMsg.replace("X", msgSubject);
-        msgObj[getKeyByValue(stringMsgTypeArr, msgIncludeIdx)] = validationMsg;
+            let validationMsg = stringMsgs[msgIncludeIdx];
+            validationMsg = validationMsg.replace("XX", msgSubject);
+            msgObj[getKeyByValue(stringMsgTypeArr, msgIncludeIdx)] = validationMsg;
         }
         if(msgType === msgTypes.number && msgIncludeIndicatorArr[msgIncludeIdx]){
-        let validationMsg = stringMsgs[msgIncludeIdx];
-        validationMsg = validationMsg.replace("X", msgSubject);
-        validationMsg = validationMsg.replace("Y", msgLimitVarArr[msgIncludeIdx]);
-        validationMsg = validationMsg.replace("Z", typeUnits[msgTypes.number]);
-        msgObj[getKeyByValue(numberMsgTypeArr, msgIncludeIdx)] = validationMsg;
+            let validationMsg = stringMsgs[msgIncludeIdx];
+            validationMsg = validationMsg.replace("XX", msgSubject);
+            validationMsg = validationMsg.replace("YY", msgLimitVarArr[msgIncludeIdx]);
+            validationMsg = validationMsg.replace("ZZ", typeUnits[msgTypes.number]);
+            msgObj[getKeyByValue(numberMsgTypeArr, msgIncludeIdx)] = validationMsg;
         }
     }
     return msgObj;
--- src/validation/registerValidation.js
+++ src/validation/registerValidation.js
@@ -5,26 +5,14 @@ import validation from "./validation";
 import generateMessages from "./msgGenerationUtil";
 
 const registerSchema = Joi.object({
-  firstName: Joi.string().min(2).max(100).required().messages({
-    "string.min": "First Name should be at least 2 characters long",
-    "string.max": "First Name should be at most 100 characters long",
-    "string.empty": "First Name cannot be empty"
-  }),
-  middleName: Joi.string().min(2).max(100).required().allow("").messages({
-    "string.min": "Middle Name should be at least 2 characters long",
-    "string.max": "Middle Name should be at most 100 characters long",
-    "string.empty": "Middle Name cannot be empty"
-  }),
-  lastName: Joi.string().min(2).max(100).required().messages({
-    "string.min": "Last Name should be at least 2 characters long",
-    "string.max": "Last Name should be at most 100 characters long",
-    "string.empty": "Last Name cannot be empty"
-  }),
-  phone: Joi.string().min(7).max(12).required().messages({
-    "string.min": "Phone should be at least 2 characters long",
-    "string.max": "Phone should be at most 100 characters long",
-    "string.empty": "Phone cannot be empty"
-  }),
+  firstName: Joi.string().min(2).max(100).required().messages(generateMessages("First Name", [2,100], 0,
+  [1,1,1])),
+  middleName: Joi.string().min(2).max(100).required().allow("").messages(generateMessages("Middle Name", [2,100], 0,
+  [1,1,1])),
+  lastName: Joi.string().min(2).max(100).required().messages(generateMessages("Last Name", [2,100], 0,
+  [1,1,1])),
+  phone: Joi.string().min(7).max(12).required().messages(generateMessages("Phone", [7,12], 0,
+  [1,1,1])),
   email: Joi.string()
     .email({ tlds: { allow: false } })
     .required(),
@@ -32,53 +20,26 @@ const registerSchema = Joi.object({
     .pattern(new RegExp("^(?=.*[A-Z])(?=.*[a-z]).{0,}$"))
     .min(6)
     .max(1024)
-    .required().messages({
-    "string.min": "Password should be at least 6 characters long",
-    "string.max": "Password should be at most 1024 characters long",
-    "string.empty": "Password cannot be empty",
-    "string.pattern.base": "Password should meet rules",
-  }),
+    .required().messages(generateMessages("Password", [6,1024], 0,
+  [1,1,1,1])),
   imageUrl: Joi.string().min(6).max(1024).allow("").messages(generateMessages("Image URL", [6,1024], 0,
   [1,1])),
-  imageAlt: Joi.string().min(6).max(1024).allow("").messages({
-    "string.min": "Image ALT should be at least 6 characters long",
-    "string.max": "Image ALT should be at most 1024 characters long"
-  }),
-  state: Joi.string().min(2).max(255).allow("").messages({
-    "string.min": "State should be at least 2 characters long",
-    "string.max": "State should be at most 255 characters long"
-  }),
-  country: Joi.string().min(2).max(256).required().messages({
-    "string.min": "Country should be at least 2 characters long",
-    "string.max": "Country should be at most 156 characters long",
-    "string.empty": "Country cannot be empty"
-  }),
-  city: Joi.string().min(2).max(256).required().messages({
-    "string.min": "City should be at least 2 characters long",
-    "string.max": "City should be at most 256 characters long",
-    "string.empty": "City cannot be empty"
-  }),
-  street: Joi.string().min(2).max(256).required().messages({
-    "string.min": "Street should be at least 2 characters long",
-    "string.max": "Street should be at most 256 characters long",
-    "string.empty": "Street cannot be empty"
-  }),
-  houseNumber: Joi.string().min(1).max(256).required().messages({
-    "string.min": "House Number should be at least 1 characters long",
-    "string.max": "House Number be at most 256 characters long",
-    "string.empty": "House Number cannot be empty"
-  }),
-  zipCode: Joi.number().min(1).max(256).allow("").messages({
-    "number.min": "Zip Code should be at least 1 digit long",
-    "number.max": "Zip Code be at most 256 digits long"
-  }),
+  imageAlt: Joi.string().min(6).max(1024).allow("").messages(generateMessages("Image ALT", [6,1024], 0,
+  [1,1])),
+  state: Joi.string().min(2).max(255).allow("").messages(generateMessages("State", [2,255], 0,
+  [1,1])),
+  country: Joi.string().min(2).max(256).required().messages(generateMessages("Country", [2,156], 0,
+  [1,1,1])),
+  city: Joi.string().min(2).max(256).required().messages(generateMessages("City", [2,256], 0,
+  [1,1,1])),
+  street: Joi.string().min(2).max(256).required().messages(generateMessages("Street", [2,256], 0,
+  [1,1,1])),
+  houseNumber: Joi.string().min(1).max(256).required().messages(generateMessages("House Number", [1,256], 0,
+  [1,1,1])),
+  zipCode: Joi.number().min(1).max(256).allow("").messages(generateMessages("Zip Code", [1,256], 1,
+  [1,1])),
   biz: Joi.boolean(),
 });
-
-// //{
-//     "string.min": "Image URL should be at least 6 characters long",
-//     "string.max": "Image URL should be at most 1024 characters long"
-//   }
 
 const validateRegisterSchema = (userInput) =>
   validation(registerSchema, userInput);
