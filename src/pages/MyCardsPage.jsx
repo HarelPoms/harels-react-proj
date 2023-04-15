006aee48fbf06a99efaed34ebc8b1efaac18fa42
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-04-15 11:00:59 +0300

    Making display more responsive

--- src/pages/MyCardsPage.jsx
+++ src/pages/MyCardsPage.jsx
@@ -105,7 +105,7 @@ const MyCardsPage = () => {
         <Box>
         <Grid container spacing={2}>
             {cardsArr.map((item) => (
-            <Grid item xs={4} key={item._id + Date.now()}>
+            <Grid item xs={6} md={4} key={item._id + Date.now()}>
                 <CardComponent
                 id={item._id}
                 title={item.title}
