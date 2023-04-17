bc2675e9a1f854884b1afa8a3c687aced7398cd8
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-04-17 15:18:05 +0300

    About Page Draft

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -15,12 +15,24 @@ const AboutPage = () => {
         <Box component="div" noValidate sx={{ mt: 3 }}>
             <Grid container spacing={2}>
                 {/* Row one */}
-                <Grid item xs={6} sm={6} md={6}>
-                    <Img alt="test" src={require("../assets/images/CardPreview1.PNG")} />
+                <Grid item xs={5} sm={5} sx={{mt:5, ml:1}}>
+                    <Typography sx={{bgcolor:"limegreen"}}>This is a business card, including an image, brief subtitle and description.beneath it, is a row containing the myriad of actions your user may perform.
+                    The Edit button allows you to edit the business card only if the card was created by you as a business user or admin.
+                    The delete button is available if you're either an Admin, or you created the card as a business user. 
+                    </Typography>
                 </Grid>
-                <Grid item xs={6} sm={6}>
-                    <Typography>This is how we do it</Typography>
-                </Grid>  
+                <Grid item xs={6} sm={6} md={6} >
+                    <Img alt="Card With Actions for biz/admin" src={require("../assets/images/CardPreview1.PNG")} />
+                </Grid>
+                {/* Row Two */}
+                <Grid item xs={5} sm={5} sx={{mt:5, ml:1}}>
+                    <Typography sx={{bgcolor:"limegreen"}}>
+                        You may also perform like/unlike via the button available if you are a normal user
+                    </Typography>
+                </Grid>
+                <Grid item xs={6} sm={6} md={6} >
+                    <Img alt="test" src={require("../assets/images/CardPreview2.PNG")} />
+                </Grid>
             </Grid>
         </Box>
     );
