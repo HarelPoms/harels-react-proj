adce9c371604b1060a6de71b1561e19efb7d48ed
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-04-18 17:17:02 +0300

    Footer Prototype

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ import {
   CssBaseline,
   CircularProgress,
 } from "@mui/material";
-
+import Box from "@mui/material/Box";
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import InfoIcon from '@mui/icons-material/Info';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import AttributionIcon from '@mui/icons-material/Attribution';
 /* toast */
 import { ToastContainer } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
 import "./App.css";
@@ -16,6 +22,7 @@ import MuiNavbar from "./components/Navbar/MuiNavbar";
 import Router from "./routes/Router";
 import { useSelector } from "react-redux";
 import useLoggedIn from "./hooks/useLoggedIn";
+import ROUTES from "./routes/ROUTES";
 
 const light = {
   palette: {
@@ -31,6 +38,8 @@ const dark = {
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [value, setValue] = useState(0);
+  const navigate = useNavigate();
   const loggedIn = useLoggedIn();
   useEffect(() => {
     (async () => {
@@ -42,6 +51,15 @@ function App() {
   const isDarkTheme = useSelector(
     (bigPie) => bigPie.darkThemeSlice.isDarkTheme
   );
+  const handleToAboutClick = () => {
+    navigate(ROUTES.ABOUT);
+  }
+  const handleToFavsClick = () => {
+    navigate(ROUTES.MYFAVS);
+  }
+  const handleToMyCardsClick = () => {
+    navigate(ROUTES.MYCARDS);
+  }
   return (
     <ThemeProvider theme={isDarkTheme ? createTheme(dark) : createTheme(light)}>
       <CssBaseline />
@@ -62,7 +80,21 @@ function App() {
           <MuiNavbar />
         </header>
         <main>{isLoading ? <CircularProgress /> : <Router />}</main>
-        <footer></footer>
+        <footer>
+          <Box>
+            <BottomNavigation
+              showLabels
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+            >
+              <BottomNavigationAction label="About" icon={<InfoIcon />} onClick={handleToAboutClick}/>
+              <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} onClick={handleToFavsClick}/>
+              <BottomNavigationAction label="My Cards" icon={<AttributionIcon />} onClick={handleToMyCardsClick}/>
+            </BottomNavigation>
+          </Box>
+        </footer>
       </Container>
     </ThemeProvider>
   );
